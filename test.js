cac5e83b84ed33a0d26276de1251a0536a57d8a6
@vishwajeetsharma09 <2000031822@kluniversity.in>
2024-07-10 00:58:04 +0530

    initial

--- test.js
+++ test.js
@@ -1,24 +1,26 @@
+// import dotenv from 'dotenv';
+// dotenv.config();
 
-import { PrismaClient } from '@prisma/client';
+// import { PrismaClient } from '@prisma/client';
+// const prisma = new PrismaClient();
 
-const prisma = new PrismaClient();
+// async function main() {
+//   const referral = await prisma.referral.create({
+//     data: {
+//       referrerName: "Test Referrer",
+//       referrerEmail: "referrer@test.com",
+//       refereeName: "Test Referee",
+//       refereeEmail: "referee@test.com",
+//     },
+//   });
+//   console.log(referral);
+// }
 
-async function main() {
-  const referral = await prisma.referral.create({
-    data: {
-      referrerName: 'Test Referrer',
-      referrerEmail: 'referrer@test.com',
-      refereeName: 'Test Referee',
-      refereeEmail: 'referee@test.com',
-    },
-  });
-  console.log(referral);
-}
-
-main()
-  .catch((e) => {
-    throw e;
-  })
-  .finally(async () => {
-    await prisma.$disconnect();
-  });
+// main()
+//   .catch((e) => {
+//     console.error(e);
+//     process.exit(1);
+//   })
+//   .finally(async () => {
+//     await prisma.$disconnect();
+//   });
